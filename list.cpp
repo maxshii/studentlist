d62bc896f1876885a2845ae70e0a1564549b527d
maxshii <shimax@gmail.com>
2021-11-07 20:26:39 -0800

    Update list.cpp

--- list.cpp
+++ list.cpp
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Online C++ Compiler.
-Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <iostream>
 
 #include <vector>
@@ -40,35 +32,40 @@ main() {
   vector < Student * > * vectorPointer = new vector < Student * > ();
   vectorPointer = & studentList;
 
-  Student * baka = new Student();
-  strcpy(baka -> first, "Max");
-  strcpy(baka -> last, "Shi");
-  baka -> id = 42;
-  baka -> gpa = 5.0;
-  (vectorPointer) -> push_back(baka);
-
-  print(vectorPointer);
-
-  add(vectorPointer);
-  //del(vectorPointer, 42);
-  print(vectorPointer);
-
-  cout <<(&vectorPointer[0]) << '\n' << &((*vectorPointer)[0]->first) << '\n';
-  cout << &baka << '\n' << &(baka->first) << '\n';
-  cout << &(baka->last) << '\n' << &(baka->id) ;
-  delete baka;
-  
-  cout << baka->first;
+  while(true) {
+      char input[80];
+      cin.getline(input, 80, '\n');
+      if (strcmp(input, "ADD")==0)
+      {
+          add(vectorPointer);
+      }
+      else if (strcmp(input, "PRINT")==0)
+      {
+          print(vectorPointer);
+      }
+      else if (strcmp(input, "DELETE")==0)
+      {
+          int id = 0;
+          cout << "Enter ID: ";
+          cin >> id;
+          del(vectorPointer, id);
+      }
+      else if (strcmp(input, "QUIT")==0)
+      {
+          break;   
+      }
+  }
+ 
   return 0;
 }
 
 void
 add(vector < Student * > * v) {
-  char first[80] = "fdas";
-  char last[80] = "fff";
-  int id = 1;
-  float gpa = 2;
-  /*cout << "Enter first name: ";
+  char first[80];
+  char last[80];
+  int id;
+  float gpa;
+  cout << "Enter first name: ";
   cin >> first;
   cout << "Enter last name: ";
   cin >> last;
@@ -76,7 +73,7 @@ add(vector < Student * > * v) {
   cin >> id;
   cout << "Enter gpa: ";
   cin >> gpa;
-  */
+  
 
   Student * a = new Student();
   strcpy(a -> first, first);
@@ -103,9 +100,10 @@ void del(vector < Student * > * v, int i) {
   int count = 0;
   for (ptr = v -> begin(); ptr < v -> end(); ++ptr) {
     if (( * ptr) -> id == i) {
-
-      v -> erase(ptr);
       
+      delete *ptr;
+    
+      v -> erase(ptr);
     }
 
     count++;
